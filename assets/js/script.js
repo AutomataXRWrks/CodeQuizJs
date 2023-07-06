753133546bbd27fbb390d09d04fcbb68c6336fa6
akautomatadev <automatalab.contacto@gmail.com>
2023-07-05 18:15:11 -0600

    init template

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,9 @@
 var timerText = document.querySelector('#timer');
-var questionContainer = document.querySelector('.answerButtons');
+var optionsContainer = document.querySelector('.answerButtons');
 var questions = document.querySelector('.questions');
-
-
-
+var questionContainer = document.querySelector('.question-container');
+var initButton = document.querySelector('.start');
+var initSubTitle = document.querySelector('.init-text')
 
 var codeQuiz  = {
     scene: 1,
@@ -47,9 +47,9 @@ function setTime() {
     for (var i = 0; i < 4; i++) {
       var buttons = document.createElement('button');
       buttons.textContent = codeQuiz.codeQuizAnswersTextO[i];
-      questions.setAttribute("style" , "font-size: 60px")
+      questions.setAttribute("style" , "font-size: 55px", "text-align: left");
       //buttons.setAttribute("style" , "border-radius: 15%; font-size: 25px; margin: 1rem; background-color: blueviolet; display: block;");
-      questionContainer.appendChild(buttons);
+      optionsContainer.appendChild(buttons);
     } 
 
   } else if(scene === 1){
@@ -57,9 +57,9 @@ function setTime() {
       var buttons = document.createElement('button');
       buttons.textContent = codeQuiz.codeQuizAnswersTextT[i];
       console.log(codeQuiz.codeQuizAnswersTextT[i]);
-      questions.setAttribute("style" , "font-size: 55px")
+      questions.setAttribute("style" , "font-size: 55px", "text-align: left");
       //buttons.setAttribute("style" , "border-radius: 15%; font-size: 25px; margin: 1rem; background-color: blueviolet; display: block;");
-      questionContainer.appendChild(buttons);  
+      optionsContainer.appendChild(buttons);  
     }
   }else if (scene === 2){
     for (var i = 0; i < 4; i++){
@@ -67,52 +67,68 @@ function setTime() {
       buttons.textContent = codeQuiz.codeQuizAnswersTextTH[i];
       console.log(codeQuiz.codeQuizAnswersTextTH[i]);
       questions.setAttribute("style" , "font-size: 60px")
+      buttons.setAttribute("style", "width: 31%;")
       //buttons.setAttribute("style" , "border-radius: 15%; font-size: 25px; margin: 1rem; background-color: blueviolet; display: block;");
-      questionContainer.appendChild(buttons); 
+      optionsContainer.appendChild(buttons); 
   }
   } else if (scene === 3){
     for (var i = 0; i < 4; i++){
       var buttons = document.createElement('button');
       buttons.textContent = codeQuiz.codeQuizAnswersTextF[i];
       console.log(codeQuiz.codeQuizAnswersTextF[i]);
-      questions.setAttribute("style" , "font-size: 60px")
+      questions.setAttribute("style" , "font-size: 51px")
       //buttons.setAttribute("style" , "border-radius: 15%; font-size: 25px; margin: 1rem; background-color: blueviolet; display: block;");
-      questionContainer.appendChild(buttons); 
+      optionsContainer.appendChild(buttons); 
   }
   } else if (scene === 4){
     for (var i = 0; i < 4; i++){
       var buttons = document.createElement('button');
-      buttons.textContent = codeQuiz.codeQuizAnswersTextF[i];
+      buttons.textContent = codeQuiz.codeQuizAnswersTextFV[i];
       console.log(codeQuiz.codeQuizAnswersTextFV[i]);
-      questions.setAttribute("style" , "font-size: 60px")
+      questions.setAttribute("style" , "font-size: 46px")
       //buttons.setAttribute("style" , "border-radius: 15%; font-size: 25px; margin: 1rem; background-color: blueviolet; display: block;");
-      questionContainer.appendChild(buttons); 
+      optionsContainer.appendChild(buttons); 
   }
   } 
 }
 
   function createQuestions(scene){
-    if(scene === 0){
-      questions.textContent = codeQuiz.questions[scene];
-      //buttons.setAttribute("style" , "border-radius: 15%; font-size: 25px; margin: 1rem; background-color: blueviolet; display: block;");  
-    } else if (scene === 1){
-      questions.textContent = codeQuiz.questions[scene];     
-    } else if (scene === 2){
-      questions.textContent = codeQuiz.questions[scene];     
-    } else if (scene === 3){
-      questions.textContent = codeQuiz.questions[scene];     
-    } else if (scene === 4){
-      questions.textContent = codeQuiz.questions[scene];     
-    }
-    
+    setTime();
+    questions.textContent = "";
+    initSubTitle.textContent = "";
+    initButton.remove();
+      if(scene === 0){
+        questions.textContent = codeQuiz.questions[scene];
+        //buttons.setAttribute("style" , "border-radius: 15%; font-size: 25px; margin: 1rem; background-color: blueviolet; display: block;");  
+      } else if (scene === 1){
+        questions.textContent = codeQuiz.questions[scene];     
+      } else if (scene === 2){
+        questions.textContent = codeQuiz.questions[scene];     
+      } else if (scene === 3){
+        questions.textContent = codeQuiz.questions[scene];     
+      } else if (scene === 4){
+        questions.textContent = codeQuiz.questions[scene];     
+      }  
   }
 
-  function init(){
-    setTime();
+
+initButton.addEventListener('click', function(event){
+  event.preventDefault();
+  console.log(event);
     createButtons(codeQuiz.scene);
     createQuestions(codeQuiz.scene);
+});
+
+  function init(){
+    //setTime();
+    //createButtons(codeQuiz.scene);
+    //createQuestions(codeQuiz.scene);
 
   }
 
+  function levelLogic(){
+    console.log('level logic');
+
+  }
 
   init();
